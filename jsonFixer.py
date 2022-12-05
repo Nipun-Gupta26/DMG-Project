4bb2baf88888559d9980258f565be465640d813d
Yuvraj Singh <yuvrajsingh808@gmail.com>
2022-12-05 14:18:48 +0530

    added location columns

--- jsonFixer.py
+++ jsonFixer.py
@@ -24,34 +24,77 @@ import json
 #     json.dump(restaurants, f, indent=4)
 
 # # read from zomatoData.json
-with open('zomatoData.json', 'r') as f:
-    restaurants = json.load(f)
+restaurants = None
 
+
+def readJson():
+    global restaurants
+    with open('zomatoData.json', 'r') as f:
+        restaurants = json.load(f)
 # convert json to csv
-with open('zomatoData.csv', 'w') as f:
-    f.write('Name,Address,Phone,Rating,AvgCost,Known For,Popular Dishes,Cuisine,Reviews\n')
-    for link in restaurants:
-        for restaurant in restaurants[link]:
-            f.write(restaurant['name'] + ',')
-            f.write("\"" + restaurant['address'] + "\"" + ',')
-            f.write(restaurant['phone'] + ',')
-            f.write(restaurant['rating'] + ',')
-            f.write(restaurant['avgCost'] + ',')
-            temp = ''
-            for item in restaurant['knownFor']:
-                temp += item + '|'
-            f.write(temp + ',')
-            temp = ''
-            for item in restaurant['popularDishes']:
-                temp += item + '|'
-            f.write(temp + ',')
-            temp = ''
-            for item in restaurant['cuisine']:
-                temp += item + '|'
-            f.write(temp + ',')
-            temp = ''
-            for item in restaurant['reviewHighlights']:
-                temp += item + '|'
-            f.write(temp + '\n')
+
+
+def convertJsonToCsv():
+    with open('zomatoData.csv', 'w') as f:
+        f.write(
+            'Name,Address,Phone,Rating,AvgCost,Known For,Popular Dishes,Cuisine,Reviews\n')
+        for link in restaurants:
+            for restaurant in restaurants[link]:
+                f.write(restaurant['name'] + ',')
+                f.write("\"" + restaurant['address'] + "\"" + ',')
+                f.write(restaurant['phone'] + ',')
+                f.write(restaurant['rating'] + ',')
+                f.write(restaurant['avgCost'] + ',')
+                temp = ''
+                for item in restaurant['knownFor']:
+                    temp += item + '|'
+                f.write(temp + ',')
+                temp = ''
+                for item in restaurant['popularDishes']:
+                    temp += item + '|'
+                f.write(temp + ',')
+                temp = ''
+                for item in restaurant['cuisine']:
+                    temp += item + '|'
+                f.write(temp + ',')
+                temp = ''
+                for item in restaurant['reviewHighlights']:
+                    temp += item + '|'
+                f.write(temp + '\n')
 
             # f.write(restaurant['link'] + '\n')
+
+
+readJson()
+
+def addLocationColumn():
+    lenths = []
+    locations = ['South Delhi', 'North Delhi', 'East Delhi', 'West Delhi', 'Central Delhi']
+    locationLinkMap = {}
+    i = 0
+    links = restaurants.keys()
+    for link in restaurants:
+        lenths.append(len(restaurants[link]))
+        locationLinkMap[link] = locations[i]
+        i += 1
+        
+    print(lenths)
+    print(locationLinkMap)
+    for link in restaurants:
+        for restaurant in restaurants[link]:
+            restaurant['location'] = locationLinkMap[link]
+    
+
+    with open('zomatoData.csv', 'r') as f:
+        lines = f.readlines()
+        lines[0] = lines[0].strip() + ',Location\n'
+        i = 1
+        for link in restaurants:
+            for restaurant in restaurants[link]:
+                lines[i] = lines[i].strip() + ',' + restaurant['location'] + '\n'
+                i += 1
+
+    with open('zomatoData.csv', 'w') as f:
+        f.writelines(lines)
+
+addLocationColumn()
